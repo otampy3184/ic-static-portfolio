1409b437e6dee687a950d8d2aa66cda4dc5c5e8e
django_freeman <o.tampodo.t.18@gmail.com>
2022-10-25 16:14:39 +0900

    Header作成

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,12 @@
+<header class="bg-blue-400 p-4 sticky top-0">
+    <nav class="container flex justify-between items-center mx-auto">
+        <!-- Portfolioのタイトル-->
+        <div class="text-2xl">MY PORTFOLIO</div>
+        <div class="space-x-12">
+            <!--各要素ページへのリンク-->
+            <a href="#home" class="hover:text-gray-500">HOME</a>
+            <a href="#about" class="hover:text-gray-500">ABOUT</a>
+            <a href="#portfolio" class="hover:text-gray-500">PORTFOLIO</a>
+        </div>
+    </nav>
+</header>
